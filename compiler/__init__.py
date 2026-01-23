71bc2540e8f3475a39ac85f2d0c2e49b86430c6d
Dimius0 <superperson1@ya.ru>
2026-01-23 14:46:15 +0300

    Update __init__.py

--- compiler/__init__.py
+++ compiler/__init__.py
@@ -1,1 +1,13 @@
+"""
+SpectraVortex Compiler
+Photonic Programming Language
+"""
 
+__version__ = "0.1.0"
+__author__ = "SpectraVortex Team"
+__license__ = "MIT"
+
+
+def hello():
+    """Simple test function"""
+    return "SpectraVortex Compiler v0.1.0"
