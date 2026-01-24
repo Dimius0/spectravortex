43909c376a772a08e7f277ce2da17f6d40bd470b
Dimius0 <superperson1@ya.ru>
2026-01-25 00:47:04 +0300

    Update __init__.py

--- simulator/__init__.py
+++ simulator/__init__.py
@@ -11,3 +11,10 @@ __license__ = "MIT"
 def hello():
     """Simple test function"""
     return "SpectraVortex Simulator v0.1.0"
+# spectravortex/simulator/__init__.py
+
+# from .elements import OpticalElement
+# from .fields import OpticalField
+# Добавьте сюда импорт основных классов симулятора
+
+# __all__ = ["OpticalElement", "OpticalField"]
